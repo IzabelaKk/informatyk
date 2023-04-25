69d2b7faeff3ea748d1eb5c909212a0d0fa4d60f
IzabelaKk <izabelakurdej@op.pl>
2023-04-25 14:52:55 +0200

    dms i xyz2flh
    jeszcze nie skonczone

--- script_git.py
+++ script_git.py
@@ -30,3 +30,49 @@ class transformacje():
             self.f = (self.a - self.b) / self.a
             self.e = sqrt(2 * self.f - self.f ** 2) 
             self.e2 = (2 * self.f - self.f ** 2) 
+            
+            
+        def dms(x,txt):
+            """
+            Funkcja przeliczająca wartość wyrażoną w radianach na wartość wyrażoną w stopniach, minutach i sekundach
+            ----------
+            x : FLOAT
+                 wartość w radianach
+
+            Returns
+            -------
+            lat
+                [stopnie dziesiętne] - szerokość geodezyjna
+            lon
+                [stopnie dziesiętne] - długośc geodezyjna.
+            h : TYPE
+                [metry] - wysokość elipsoidalna
+            output [STR] - optional, defoulf 
+                dec_degree - decimal degree
+                dms - degree, minutes, sec
+            """
+            sig = ' '
+            if x < 0:
+                sig = '-'
+                x = abs(x)
+            x = x * 180/pi
+            d = int(x)
+            m = int(60 * (x - d))
+            s = (x - d - m/60)*3600
+            print(txt,sig,'%3d' % d,'°', '%2d' % m,"'",'%7.5f' % s,'"')
+            
+            
+        def XYZ2flh(self, X, Y, Z):
+            p = np.sqrt(X**2 + Y**2)
+            f = np.arctan(Z/(p*(1-self.e2)))
+            dms(f)
+            while True:
+                N = Np(f, self.a, self.e2)
+                h = (p/np.cos(f))-N
+                fpop = f
+                f = np.arctan(Z/(p*(1-self.e2*N/(N+h))))
+                dms(f)
+                if abs(fpop-f) < (0.000001/206265):
+                    break
+            l = np.arctan2(Y,X)
+            return(f,l,h)
