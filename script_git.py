e82246d56649c477ea5ce760ff9f47df00b504de
IzabelaKk <izabelakurdej@op.pl>
2023-04-27 13:22:11 +0200

    Update script_git.py

--- script_git.py
+++ script_git.py
@@ -1,5 +1,7 @@
 from math import *
 import numpy as np
+import argparse
+
 
 class transformacje():
     
@@ -110,8 +112,8 @@ class transformacje():
         self.dms('f', f)
         while True:
             N = self.Np()
-            h = (p/np.cos(f))-N
             fpop = f
+            h = (p/np.cos(f))-N
             fl = np.arctan(Z/(p*(1-self.e2*N/(N+h))))
             self.dms('f', f)
             if abs(fpop-f) < (0.000001/206265):
@@ -144,48 +146,49 @@ class transformacje():
         Y = (N + h) * cos(f) * sin(l)
         Z = (N * (1 - self.e2) + h) * sin(f)
         return(X,Y,Z)
+    
         def u1992(self, f, l):
-        """
-        Odwzorowanie Gausa Krugera do układu 1992. Odnosi się do południka osiowego 19 stopni. 
-        Współrzędne wejsciowe zostają odwzorowane na do układu lokalnego GK a następnie przeskalowane zgodnie z układem 1992.
-        Parametry:
-        ---------
-        f:  FLOAT
-            szerokoć geodezyjna
-        l:  FLOAT
-            długoć geodezyjna 
-        Returns:
-        ---------
-        x:  FLOAT
-            szerokosc prostkątna lokalna
-        y:  FLOAT
-            długosc prostokątna lokalna
-         
+            """
             
-        """
-        m = 0.9993
-        N = self.Np()
-        t = np.tan(f)
-        e_2 = self.e2/(1-self.e2)
-        n2 = e_2 * (np.cos(f))**2
+            Odwzorowanie Gausa Krugera do układu 1992. Odnosi się do południka osiowego 19 stopni. 
+            Współrzędne wejsciowe zostają odwzorowane na do układu lokalnego GK a następnie przeskalowane zgodnie z układem 1992.
+            Parametry:
+            ---------
+            f:  FLOAT
+                szerokoć geodezyjna
+            l:  FLOAT
+                długoć geodezyjna 
+            Returns:
+            ---------
+            x:  FLOAT
+                szerokosc prostkątna lokalna
+            y:  FLOAT
+                długosc prostokątna lokalna
+
+            """
+            m = 0.9993
+            N = self.Np()
+            t = np.tan(f)
+            e_2 = self.e2/(1-self.e2)
+            n2 = e_2 * (np.cos(f))**2
         
-        l0 = 19 * np.pi / 180
-        d_l = l - l0
+            l0 = 19 * np.pi / 180
+            d_l = l - l0
         
-        A0 = 1 - (self.e2/4) - ((3*(self.e2**2))/64) - ((5*(self.e2**3))/256)   
-        A2 = (3/8) * (self.e2 + ((self.e2**2)/4) + ((15 * (self.e2**3))/128))
-        A4 = (15/256) * (self.e2**2 + ((3*(self.e2**3))/4))
-        A6 = (35 * (self.e2**3))/3072 
+            A0 = 1 - (self.e2/4) - ((3*(self.e2**2))/64) - ((5*(self.e2**3))/256)   
+            A2 = (3/8) * (self.e2 + ((self.e2**2)/4) + ((15 * (self.e2**3))/128))
+            A4 = (15/256) * (self.e2**2 + ((3*(self.e2**3))/4))
+            A6 = (35 * (self.e2**3))/3072 
         
-        sigma = a * ((A0*f) - (A2*np.sin(2*f)) + (A4*np.sin(4*f)) - (A6*np.sin(6*f)))
+            sigma = a * ((A0*f) - (A2*np.sin(2*f)) + (A4*np.sin(4*f)) - (A6*np.sin(6*f)))
         
-        xgk = sigma + ((d_l**2)/2) * N *np.sin(f) * np.cos(f) * (1 + ((d_l**2)/12) * ((np.cos(f))**2) * (5 - t**2 + 9*n2 + 4*(n2**2)) + ((d_l**4)/360) * ((np.cos(f))**4) * (61 - (58*(t**2)) + (t**4) + (270*n2) - (330 * n2 *(t**2))))
-        ygk = d_l * (N*np.cos(f)) * (1 + ((((d_l**2)/6) * (np.cos(f))**2) * (1-t**2+n2)) +  (((d_l**4)/(120)) * (np.cos(f)**4)) * (5 - (18 * (t**2)) + (t**4) + (14*n2) - (58*n2*(t**2))))
+            xgk = sigma + ((d_l**2)/2) * N *np.sin(f) * np.cos(f) * (1 + ((d_l**2)/12) * ((np.cos(f))**2) * (5 - t**2 + 9*n2 + 4*(n2**2)) + ((d_l**4)/360) * ((np.cos(f))**4) * (61 - (58*(t**2)) + (t**4) + (270*n2) - (330 * n2 *(t**2))))
+            ygk = d_l * (N*np.cos(f)) * (1 + ((((d_l**2)/6) * (np.cos(f))**2) * (1-t**2+n2)) +  (((d_l**4)/(120)) * (np.cos(f)**4)) * (5 - (18 * (t**2)) + (t**4) + (14*n2) - (58*n2*(t**2))))
         
-        x92 = m*xgk - 5300000
-        y92 = m*ygk + 500000
+            x92 = m*xgk - 5300000
+            y92 = m*ygk + 500000
         
-        return (x92, y92)
+            return (x92, y92)
     
     def u2000(self, f, l):
         """
@@ -247,6 +250,30 @@ class transformacje():
          
         return(x00, y00)
     
+    def XYZ2neu(self, dXYZ, f, l):
+        """
+        
+
+        Parameters
+        ----------
+        dXYZ : TYPE
+            DESCRIPTION.
+        f : TYPE
+            DESCRIPTION.
+        l : TYPE
+            DESCRIPTION.
+
+        Returns
+        -------
+        None.
+
+        """
+        Rneu = np.array([[-np.sin(f) * np.cos(l), -np.sin(l), np.cos(f) * cos(l)],
+                      [-np.sin(f) * np.sin(l), np.cos(l), np.cos(f) * np.sin(l)],
+                      [np.cos(f), 0, np.sin(f)]])
+        
+        R = Rneu(f,l)
+        return(R.T @ dXYZ)
     
     
 if __name__ == "__main__":
@@ -257,4 +284,23 @@ if __name__ == "__main__":
 
 #if __name__ == "__main__":
  #   orto = transformacje(model = "wgs84")
+ 
+ 
+ 
+if __name__ == "__main__":
+    
+     trans = transformacje()
+     pars = argparse.ArgumentParser(description = "Transformacje współrzędnych")
+     pars.add_argument(dest = "Metoda", metavar = 'M', nargs = 1, type = str,
+                       help = 'napisz nazwe metody (wymienic metody)')
+     pars.add_argument(dest = 'Załączanie danych', metavar = 'L', nargs = 1, type = str,
+                       help = 'jak chcesz wprowadzac tane txt, input')
+     
+     pars.add_argument(dest = 'dane', metavar = 'D', type = float, nargs = '+',
+                       help = 'wsp do zamiany')
+     
+     args = pars.parse_args()
+     print(args)
+     funkcja = getattr(trans, args.method[0])
+     
     
