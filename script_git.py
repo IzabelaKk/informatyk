6af6f0a75c7ee8da4fcce750536578b0d2f76d9b
IzabelaKk <izabelakurdej@op.pl>
2023-06-05 14:57:55 +0200

    Update script_git.py

--- script_git.py
+++ script_git.py
@@ -145,7 +145,7 @@ class transformacje():
                 N = self.a / np.sqrt(1- self.e2 * np.sin(f)**2)
                 h = (p / np.cos(f)) - N
                 fpop = f
-                fl = np.arctan(Z/(p*(1-self.e2*N/(N+h))))
+                f = np.arctan(Z/(p*(1-self.e2*N/(N+h))))
                 if abs(fpop-f) < (0.000001/206265):
                     break
             l = np.arctan2(Y,X)
